5e21d709694ea8cd9795ce0f1a08f955ca1a956e
MAHERoff <121495196+MAHERoff@users.noreply.github.com>
2023-11-26 23:11:08 +0000

    Add app.py

--- app.py
+++ app.py
@@ -9,3 +9,27 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+import random
+
+def get_user_choice():
+    user_input = input("Choose rock, paper, or scissors: ").lower()
+    while user_input not in ["rock", "paper", "scissors"]:
+        print("Invalid choice. Please enter rock, paper, or scissors.")
+        user_input = input("Choose again: ").lower()
+    return user_input
+
+def get_computer_choice():
+    return random.choice(["rock", "paper", "scissors"])
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors") or
+        (user_choice == "scissors" and computer_choice == "paper") or
+        (user_choice == "paper" and computer_choice == "rock")
+    ):
+        return "You win!"
+    else:
+        return "You lose!"
